31ebfd8cec19cd61403d7451dd1bd00f5cb261ef
Hari Krishna <Harik@touchcoresystems.com>
2020-08-15 13:16:25 +0530

    2d array hour glass.

--- HackerRank/Data Structures/Arrays.cs
+++ HackerRank/Data Structures/Arrays.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HackerRank.Data_Structures
@@ -68,5 +69,20 @@ namespace HackerRank.Data_Structures
         }
 
         #endregion https://www.hackerrank.com/challenges/arrays-ds/problem
+
+        #region https://www.hackerrank.com/challenges/2d-array/problem
+        public static int hourGlass(int[][] arr)
+        {
+            var sumCollection = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    sumCollection.Add(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+                }
+            }
+            return sumCollection.Max();
+        }
+        #endregion https://www.hackerrank.com/challenges/2d-array/problem
     }
 }
