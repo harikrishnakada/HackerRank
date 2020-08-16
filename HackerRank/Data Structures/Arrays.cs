6974c16f7c65ba51c84cdcd9959d05532a8f389e
Hari Krishna <Harik@touchcoresystems.com>
2020-08-16 20:04:45 +0530

    DYnamic array

--- HackerRank/Data Structures/Arrays.cs
+++ HackerRank/Data Structures/Arrays.cs
@@ -84,5 +84,37 @@ namespace HackerRank.Data_Structures
             return sumCollection.Max();
         }
         #endregion https://www.hackerrank.com/challenges/2d-array/problem
+
+        #region https://www.hackerrank.com/challenges/dynamic-array/problem
+        public static List<int> dynamicArray(int n, List<List<int>> queries)
+        {
+            var res = new List<int>();
+
+            var dynamicSequence = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+                dynamicSequence.Add(new List<int>());
+
+            int lastAnswer = 0;
+
+            foreach (var item in queries)
+            {
+                int x = item[1];
+                int y = item[2];
+                int seq = ((x ^ lastAnswer) % n);
+                if (item[0] == 1)
+                {
+                    dynamicSequence[seq].Add(y);
+                }
+                else if (item[0] == 2)
+                {
+                    lastAnswer = dynamicSequence[seq][y % dynamicSequence[seq].Count()];
+                    res.Add(lastAnswer);
+                }
+
+            }
+
+            return res;
+        }
+        #endregion https://www.hackerrank.com/challenges/dynamic-array/problem
     }
 }
