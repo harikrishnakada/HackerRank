c17578a60ff0326432eb5ad62b7a69650e5841be
Hari Krishna <Harik@touchcoresystems.com>
2020-08-16 20:04:32 +0530

    Interview prep warm up challenge.

---
+++ HackerRank/Interview Preparati0on Kit/WarmUpChallenges.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.Interview_Preparati0on_Kit
+{
+    public static class WarmUpChallenges
+    {
+
+        #region https://www.hackerrank.com/challenges/sock-merchant/problem
+        public static int sockMerchant(int n, int[] arr)
+        {
+            int res = 0;
+
+            var noOfColours = arr.Distinct();
+
+            foreach (var item in noOfColours)
+            {
+                var noOfPairs = arr.Where(x => x == item).Count() / 2;
+                res += noOfPairs;
+            }
+
+            return res;
+        }
+        #endregion https://www.hackerrank.com/challenges/sock-merchant/problem
+
+        #region https://www.hackerrank.com/challenges/counting-valleys/problem
+        public static int countingValleys(int n, string s)
+        {
+            int noOfValleys = 0;
+            int seaLevel = 0;
+
+            foreach (var item in s.ToCharArray())
+            {
+                if (item == 'D')
+                    seaLevel--;
+                else if (item == 'U')
+                {
+                    seaLevel++;
+                    if (seaLevel == 0)
+                        noOfValleys++;
+                }
+            }
+
+            return noOfValleys;
+        }
+        #endregion https://www.hackerrank.com/challenges/counting-valleys/problem
+    }
+}
